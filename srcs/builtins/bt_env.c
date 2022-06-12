4c3e8efe05300f1ccb6d0b1a3485898cfdce161e
xle-boul <xleboulenge@gmail.com>
2022-06-12 23:48:15 +0200

    aavancement du jour. Unset termine, export bien avance, env termine, cd termine et je pense exite. restent des modifs a apparter a pn et a export

--- srcs/builtins/bt_env.c
+++ srcs/builtins/bt_env.c
@@ -6,34 +6,35 @@
 /*   By: xle-boul <xle-boul@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 15:42:59 by xle-boul          #+#    #+#             */
-/*   Updated: 2022/06/11 19:41:40 by xle-boul         ###   ########.fr       */
+/*   Updated: 2022/06/12 23:19:13 by xle-boul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+#include <sys/stat.h>
+#include <sys/file.h>
 
-bool	is_dir(char *arg)
-{
-	int	i;
-
-	i = 0;
-	while (arg[i] != '\0')
-		i++;
-	i--;
-	printf("i = %d\nletter = %c\n", i, arg[i]); //fonction pour reperer s'il s'agit d'un directory
-	return (true);
-}
-
+// has 2 roles:
+// 	- if there is one or more arguments, it only takes into account
+// 		the first argument given. It will use the function (allowed)
+// 		stat, that refers to a struct stat to find out if the string
+// 		passed as argument is a path to an existing folder or file.
+// 		in any of those cases, it will return 126
+// 	- if there is no argument given, env will print all the variables
+// 		and their value to the stdin. each will be followed by a \n
 int	built_in_env(t_shell *shell, char **cmd_args)
 {
 	t_env	*tmp;
+	int		status;
+    struct stat stats;
 
 	if (cmd_args[1])
 	{
-		if (is_dir(cmd_args[1]) == 0)
-			printf("env: ‘%s‘: Permission denied\n", cmd_args[1]);
-		else if (is_dir(cmd_args[1]) != 0)
+		status = stat(cmd_args[1], &stats);
+		if (S_ISREG(stats.st_mode) || status != 0)
 			printf("env: ‘%s’: No such file or directory\n", cmd_args[1]);
+		else if (S_ISDIR(stats.st_mode))
+			printf("env: ‘%s‘: Permission denied\n", cmd_args[1]);
 		return (126);
 	}
 	tmp = shell->env_list;
--- srcs/builtins/bt_export.c
+++ srcs/builtins/bt_export.c
@@ -6,12 +6,14 @@
 /*   By: xle-boul <xle-boul@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 19:55:23 by xle-boul          #+#    #+#             */
-/*   Updated: 2022/06/12 12:58:15 by xle-boul         ###   ########.fr       */
+/*   Updated: 2022/06/12 23:18:53 by xle-boul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+// actual function to compares strings to one another and find the lowest ascii
+// score
 char	*find_minimum_string(t_env *head, int i, char *min, char **env)
 {
 	t_env	*tmp;
@@ -21,7 +23,7 @@ char	*find_minimum_string(t_env *head, int i, char *min, char **env)
 	{
 		if (i == 0 && ft_strncmp(tmp->data, min, ft_strlen(tmp->data)) < 0)
 			min = tmp->data;
-		else if (env[i - 1] && ft_strncmp(tmp->data, min,
+		else if (i > 0 && ft_strncmp(tmp->data, min,
 					ft_strlen(tmp->data)) < 0
 					&& ft_strncmp(env[i - 1], tmp->data,
 					ft_strlen(env[i - 1])) < 0)
@@ -31,6 +33,8 @@ char	*find_minimum_string(t_env *head, int i, char *min, char **env)
 	env[i] = ft_strdup(min);
 }
 
+// iterates through the env to find the lowest (alphabetically) unused
+// variable and copies it to the array of strings
 char	**sort_env(t_env *head, char *highest, char **env)
 {
 	int		i;
@@ -49,6 +53,16 @@ char	**sort_env(t_env *head, char *highest, char **env)
 	return (env);
 }
 
+// to print the env alphabetically, it takes a few steps:
+// 	- create a string that is likely to have the highest ascii value
+// 		to compare to the actual env variables
+// 	- create an array of strings to copy each variable and value into
+// 	- compare each element of the linked list to eachother and find
+// 		the lowest ascii score one (with ft_strncmp). Once found,
+// 		copy it the the first string of the array of strings
+// 	- iterate until all the variables are copied
+// 	- print each entry to the stdin
+// 	- free the array of pointers
 void	print_env_export(t_env *head)
 {
 	char	**sorted_env;
@@ -59,7 +73,6 @@ void	print_env_export(t_env *head)
 	sorted_env = malloc(sizeof(char *) * (list_length(head)));
 	if (!sorted_env)
 		return ;
-	sorted_env[list_length(head)] = NULL;
 	sorted_env = sort_env(head, highest, sorted_env);
 	free(highest);
 	i = 0;
@@ -72,17 +85,31 @@ void	print_env_export(t_env *head)
 	free(sorted_env);
 }
 
+// has 2 roles:
+// 	- if export doesn't get any argument: it will print the env variables
+// 		and their value prefixed with "declare -x ". These will be
+// 		alphabetically sorted.
+// 	- if export gets 1 or more arguments: if the format is x=y, it will
+// 		create a variable x of value y into the env.
 int built_in_export(t_shell *shell, char **cmd_args)
 {
-	
+	char	**args;
+	int		i;
+
+	i = 0;
 	if (!cmd_args[1])
 		print_env_export(shell->env_list);
-	// if (check_argument == 1)
-	// {
-		
-	// 	create_env_var(shell->env_list, )
-	// }
 	else
-		return (0);
+	{
+		while (cmd_args[i])
+		{
+			args = ft_split(cmd_args[1], '=');
+			while (args[i] != NULL)
+			{
+				printf("%s\n", args[i]);
+				i++;
+			}
+		}
+	}
 	return (0);
 }
